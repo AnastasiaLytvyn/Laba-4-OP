efcc07f83a617dc69b2030b68138428acdb6fb13
Anastasiia Lytvyn <lytvynana@gmail.com>
2021-04-20 16:40:01 +0300

    added compression if the number of repetitions is more than 255

--- project4/project4/RLEArchive.cpp
+++ project4/project4/RLEArchive.cpp
@@ -1,5 +1,6 @@
 #include "RLEArchive.h"
 
+//проверить для перехода на новую строку
 bool RLEArchiver::Compress(string from, string to, map <string, int> dict, map <string, int>::iterator it) {
 	ifstream input(from, ios::binary);
 	ofstream output(to, ios::binary);
@@ -19,8 +20,20 @@ bool RLEArchiver::Compress(string from, string to, map <string, int> dict, map <
 				break;
 			}
 		}
-		char countChar = countS;
-		output.write((char*)&countChar, 1);
+		if (countS < 256) {
+			char countChar = countS;
+			output.write((char*)&countChar, 1);
+		}
+		else {
+			int countS1 = countS >> 8;
+			int countS2 = countS & ((1 <<12) - 1);
+			char countChar1 = countS1;
+			char countChar2 = countS2;
+			char twoBytesAlert = 27;
+			output.write((char*)&twoBytesAlert, 1);
+			output.write((char*)&countChar1, 1);
+			output.write((char*)&countChar2, 1);
+		}
 		output.write((char*)&now, 1);
 		now = current;
 		//1They => i=0; last=1	
